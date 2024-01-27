6d22722a3656375429cbb90393a9a470ee95ddd4
vivekratr <vivekratr@gmail.com>
2024-01-27 13:28:16 +0530

    update Footer.jsx, LogoEditor.jsx, Mid.jsx and Mainpage.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,36 +1,63 @@
+import React from "react";
+
 const Footer = () => {
+  const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
+
+  const imgSM='https://cdn.discordapp.com/attachments/1096324843877703713/1200679658627280966/image.png?ex=65c70f12&is=65b49a12&hm=cb9f1263cab47830b0e9f2a1ea46dc35ae6b4876baea2de89290af193e8a6650&'
+  const imgLG='https://cdn.discordapp.com/attachments/1096324843877703713/1200680738283065395/image.png?ex=65c71013&is=65b49b13&hm=6a52197b9fff3228af9a255173ad29fc76ae125fb7b68885d1cdb7f97486c808&'
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  const isLargeDevice = windowWidth >= 768;
   return (
-    <section className="relative bg-[#FFE7B9]">
-      <div className="w-fit pb-[54px] mx-auto">
-        <div className="w-[22.38rem] mt-[96px] relative text-[1.38rem] font-500 font-inter text-dimgray text-center inline-block">
+    <section className="relative bg-[#FFE7B9] overflow-hidden">
+      <img className="min-w-[563px] w-full lg:w-[800px] lg:h-[820px] lg:left-[-40px] h-[766px] absolute bottom-0  " 
+      src={isLargeDevice ? imgLG : imgSM}
+      // src={imgSM}
+       alt="train" />
+
+<img className="w-[814px] hidden lg:block absolute   h-[834px]  bottom-0  right-0 " 
+      src='https://cdn.discordapp.com/attachments/1096324843877703713/1200680785091510322/image.png?ex=65c7101e&is=65b49b1e&hm=455c379c6b33a851ea666d5992ca1bababead89a2719034c571943be5904b81a&'
+       alt="bus" />
+      <div className="w-fit pb-[54px] lg:pb-[46px] mx-auto">
+        <div className="w-[22.38rem] lg:w-[45.06rem] mt-[96px] lg:mt-[163px] relative text-[1.38rem] lg:text-[2.44rem]  font-500 font-inter text-dimgray text-center inline-block">
           Build from City of Dreams, for the World.
         </div>
       </div>
 
-      <div className="mb-[259px]">
+      <div className="mb-[259px] lg:mb-[25px]">
         <img
-          className="w-[289px] mx-auto relative max-w-full overflow-hidden h-auto object-cover"
+          className="w-[289px] lg:w-[13.31rem] lg:h-[13.31rem] mx-auto relative max-w-full overflow-hidden h-auto object-cover"
           alt=""
           src="https://cdn.discordapp.com/attachments/1096324843877703713/1200515671629496412/image.png?ex=65c67658&is=65b40158&hm=5d6f4582d8f3c13abf62152e2e08baeb3e1e2b804ba1f099c09ceb955aa7cecb&"
         />
       </div>
 
-      <div className="mb-[57px]">
+      <div className="mb-[57px] lg:mb-[22px]">
         <img
-          className="w-[382px] mx-auto relative max-w-full overflow-hidden h-[14.75rem] object-cover"
-          alt=""
+          className="w-[382px] lg:w-[684px] lg:h-[422px] mx-auto relative max-w-full overflow-hidden h-[14.75rem] object-cover"
+          alt="ticket"
           src="https://cdn.discordapp.com/attachments/1096324843877703713/1200515751157715205/image.png?ex=65c6766b&is=65b4016b&hm=37f1f32471325e90f42b63733adb60739a428b6470aff8e30246ab6ffb5ae838&"
         />
       </div>
 
-      <div className="w-[338px] mb-[57px] mx-auto relative rounded-[5px] bg-gray box-border h-[2.81rem] overflow-hidden text-left text-[1rem] text-white font-inter border-t-[1px] border-solid border-dimgray1">
+      <div className="w-[338px] mb-[57px] lg:mb-[22px] mx-auto relative rounded-[5px] bg-gray box-border h-[2.81rem] overflow-hidden text-left text-[1rem] text-white font-inter border-t-[1px] border-solid border-dimgray1">
         <div className="absolute top-[0.81rem] left-[7.5rem] font-500">
           Share it on X
         </div>
       </div>
 
-      <div className="w-fit mx-auto pb-[78px]">
-        <div className="w-[17.06rem] relative text-[0.88rem] text-black text-center inline-block font-inter">
+      <div className="w-fit mx-auto pb-[78px] lg:pb-[37px]">
+        <div className="w-[17.06rem] lg:w-[33rem] lg:text-[1.19rem] relative text-[0.88rem] text-black text-center inline-block font-inter">
           <span>Designed And Developed by Team NovaChain for</span>
           <span className="font-semibold"> ETHMUMBAI</span>
         </div>
--- src/components/Mid.jsx
+++ src/components/Mid.jsx
@@ -1,42 +1,66 @@
 import logo from "../assets/logo.svg";
+import React from "react";
 
 const Mid = () => {
+  const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
+  const bgImageSmall = 'https://cdn.discordapp.com/attachments/1096324843877703713/1200599106100396192/image.png?ex=65c6c40c&is=65b44f0c&hm=7b36005e7b8456acc9fa39979fa622c2c041f415a028e4ca53a986ccd4d7b5b8&'
+  const bgImageLarge = 'https://cdn.discordapp.com/attachments/1096324843877703713/1200667150684274758/image.png?ex=65c7036c&is=65b48e6c&hm=b903aa59cbf024525b6f233d6021a67ae7617daa8018ee7e9de4e2b0ef3be204&'
+  const vadapavSM ='https://cdn.discordapp.com/attachments/1096324843877703713/1200515533393625189/image.png?ex=65c67637&is=65b40137&hm=948d48dbbed9972d6cfb5f162b1cef0a9b2d32c0fe26b92e55dceffba430a264&'
+  const vadapavLG ='https://cdn.discordapp.com/attachments/1096324843877703713/1200669553290661988/image.png?ex=65c705a8&is=65b490a8&hm=a60019827cd6c5cb49fadbc97b57170de8eda1288e5e20e3cc251c6dd74ec851&'
+  const chaiSM='https://cdn.discordapp.com/attachments/1096324843877703713/1200515577555460116/image.png?ex=65c67642&is=65b40142&hm=b59dc90c24fee1d47bc983559fe037e64490c161c30acf6f2a22a356705522a9&'
+  const chaiLG='https://cdn.discordapp.com/attachments/1096324843877703713/1200675000554901514/image.png?ex=65c70abb&is=65b495bb&hm=1f1f97369812429c98de662861d2946282e291ddb16e58f58581bb3ea55fb612&'
+  React.useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  const isLargeDevice = windowWidth >= 768;
   return (
     <section className="relative overflow-hidden w-full z-10 bg-[#FFE7B9]">
       <img
-        className="w-full absolute top-[88px] left-[-205px] max-w-full overflow-hidden h-[13.34rem] object-contain opacity-[0.62]"
+        className="w-full lg:w-[508px] absolute top-[88px] lg:top-[221px] lg:left-[-187px] left-[-205px] max-w-full overflow-hidden h-[13.34rem] lg:h-[324px] object-contain lg:object-cover lg:opacity-[1] opacity-[0.62]"
         alt="vadapav"
-        src="https://cdn.discordapp.com/attachments/1096324843877703713/1200515533393625189/image.png?ex=65c67637&is=65b40137&hm=948d48dbbed9972d6cfb5f162b1cef0a9b2d32c0fe26b92e55dceffba430a264&"
+        // src="https://cdn.discordapp.com/attachments/1096324843877703713/1200515533393625189/image.png?ex=65c67637&is=65b40137&hm=948d48dbbed9972d6cfb5f162b1cef0a9b2d32c0fe26b92e55dceffba430a264&"
+        src={isLargeDevice ? vadapavLG : vadapavSM}
       />
       <img
-        className="absolute  w-[200%] h-[34.75rem]  z-[-10] left-[-1px] top-[123px]"
-        src="https://cdn.discordapp.com/attachments/1096324843877703713/1200599106100396192/image.png?ex=65c6c40c&is=65b44f0c&hm=7b36005e7b8456acc9fa39979fa622c2c041f415a028e4ca53a986ccd4d7b5b8&"
-        alt=""
+        className="absolute lg:object-fill lg:w-[1760px] lg:h-[760px] w-[200%] h-[34.75rem]  z-[-10] left-[-1px] lg:left-[-1px] top-[123px] lg:top-[2.25rem]"
+        src={isLargeDevice ? bgImageLarge : bgImageSmall}
+        // src="https://cdn.discordapp.com/attachments/1096324843877703713/1200599106100396192/image.png?ex=65c6c40c&is=65b44f0c&hm=7b36005e7b8456acc9fa39979fa622c2c041f415a028e4ca53a986ccd4d7b5b8&"
+        alt="bg"
       />
       <img
-        className="w-full absolute left-[190px] top-[70px] max-w-full overflow-hidden h-[13.88rem] object-contain opacity-[0.62]"
-        alt=""
-        src="https://cdn.discordapp.com/attachments/1096324843877703713/1200515577555460116/image.png?ex=65c67642&is=65b40142&hm=b59dc90c24fee1d47bc983559fe037e64490c161c30acf6f2a22a356705522a9&"
+        className="w-full lg:w-[409px] absolute lg:left-[1233px] left-[190px]  top-[70px] lg:top-[165px] max-w-full overflow-hidden h-[13.88rem] lg:h-[409px] object-contain opacity-[0.62] lg:opacity-[0.9]"
+        alt="chai"
+        src={isLargeDevice ? chaiLG : chaiSM}
+
+        // src="https://cdn.discordapp.com/attachments/1096324843877703713/1200515577555460116/image.png?ex=65c67642&is=65b40142&hm=b59dc90c24fee1d47bc983559fe037e64490c161c30acf6f2a22a356705522a9&"
       />
-      <div className="py-[56px]">
+      <div className="py-[56px] lg:py-0 lg:mt-[69px]">
         <img
-          className="w-[215px] h-[111px] object-contain mx-auto"
+          className="w-[215px] h-[111px] lg:w-[295px] lg:h-[137px] object-contain mx-auto"
           src={logo}
           alt="logo"
         />
       </div>
-      <div className="w-fit mx-auto mt-[66px]">
-        <b className="w-[22.75rem]  relative text-[1.63rem] inline-block font-inter text-darkslategray text-center">
+      <div className="w-fit mx-auto mt-[66px] lg:mt-[64px]">
+        <b className="w-[22.75rem] lg:w-[45.31rem]  relative text-[1.63rem] lg:text-[3.06rem] inline-block font-inter text-darkslategray lg:text-[#3b3b3b] text-center">
           {`Build from `}
           <span className="[text-decoration:line-through]">Mumbai</span> Bombay,
           for the World.
         </b>
       </div>
       <div className="w-fit mx-auto">
-        <div className="w-[16.84rem] mt-[17px] font-thin relative text-[1.38rem]  text-darkslategray1 text-center ">{`29th - 31st March 2024 `}</div>
+        <div className="w-[16.84rem] lg:w-[20.31rem] mt-[17px] font-thin relative text-[1.38rem] lg:text-[1.81rem] lg:text-[#767676]  text-darkslategray1 text-center ">{`29th - 31st March 2024 `}</div>
       </div>
 
-      <div className="w-[210px] mb-[88.6px] mx-auto mt-[106px] relative rounded-[40px] bg-white box-border h-[4.19rem] overflow-hidden text-left text-[1.13rem] text-black  font-[500] border-[0.3px] border-solid border-darkgray">
+      <div className="w-[210px] mb-[88.6px] mx-auto mt-[106px] lg:mt-[140px] relative rounded-[40px] bg-white box-border h-[4.19rem] overflow-hidden text-left text-[1.13rem] text-black  font-[500] border-[0.3px] border-solid border-darkgray">
         <div
           style={{ fontWeight: 700 }}
           className="absolute top-[1.38rem] left-[2.75rem] font-inter font-300"
