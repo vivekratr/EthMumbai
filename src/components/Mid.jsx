ead3ff4950d5d7d3bef3bc1f3d451bbb8a290549
vivekratr <vivekratr@gmail.com>
2024-01-28 23:25:00 +0530

    delete 1 file and update 5 files

--- src/components/Mid.jsx
+++ src/components/Mid.jsx
@@ -3,21 +3,27 @@ import React from "react";
 
 const Mid = () => {
   const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
-  const bgImageSmall = 'https://cdn.discordapp.com/attachments/1096324843877703713/1200599106100396192/image.png?ex=65c6c40c&is=65b44f0c&hm=7b36005e7b8456acc9fa39979fa622c2c041f415a028e4ca53a986ccd4d7b5b8&'
-  const bgImageLarge = 'https://cdn.discordapp.com/attachments/1096324843877703713/1200667150684274758/image.png?ex=65c7036c&is=65b48e6c&hm=b903aa59cbf024525b6f233d6021a67ae7617daa8018ee7e9de4e2b0ef3be204&'
-  const vadapavSM ='https://cdn.discordapp.com/attachments/1096324843877703713/1200515533393625189/image.png?ex=65c67637&is=65b40137&hm=948d48dbbed9972d6cfb5f162b1cef0a9b2d32c0fe26b92e55dceffba430a264&'
-  const vadapavLG ='https://cdn.discordapp.com/attachments/1096324843877703713/1200669553290661988/image.png?ex=65c705a8&is=65b490a8&hm=a60019827cd6c5cb49fadbc97b57170de8eda1288e5e20e3cc251c6dd74ec851&'
-  const chaiSM='https://cdn.discordapp.com/attachments/1096324843877703713/1200515577555460116/image.png?ex=65c67642&is=65b40142&hm=b59dc90c24fee1d47bc983559fe037e64490c161c30acf6f2a22a356705522a9&'
-  const chaiLG='https://cdn.discordapp.com/attachments/1096324843877703713/1200675000554901514/image.png?ex=65c70abb&is=65b495bb&hm=1f1f97369812429c98de662861d2946282e291ddb16e58f58581bb3ea55fb612&'
+  const bgImageSmall =
+    "https://cdn.discordapp.com/attachments/1096324843877703713/1200599106100396192/image.png?ex=65c6c40c&is=65b44f0c&hm=7b36005e7b8456acc9fa39979fa622c2c041f415a028e4ca53a986ccd4d7b5b8&";
+  const bgImageLarge =
+    "https://cdn.discordapp.com/attachments/1096324843877703713/1200667150684274758/image.png?ex=65c7036c&is=65b48e6c&hm=b903aa59cbf024525b6f233d6021a67ae7617daa8018ee7e9de4e2b0ef3be204&";
+  const vadapavSM =
+    "https://cdn.discordapp.com/attachments/1096324843877703713/1200515533393625189/image.png?ex=65c67637&is=65b40137&hm=948d48dbbed9972d6cfb5f162b1cef0a9b2d32c0fe26b92e55dceffba430a264&";
+  const vadapavLG =
+    "https://cdn.discordapp.com/attachments/1096324843877703713/1200669553290661988/image.png?ex=65c705a8&is=65b490a8&hm=a60019827cd6c5cb49fadbc97b57170de8eda1288e5e20e3cc251c6dd74ec851&";
+  const chaiSM =
+    "https://cdn.discordapp.com/attachments/1096324843877703713/1200515577555460116/image.png?ex=65c67642&is=65b40142&hm=b59dc90c24fee1d47bc983559fe037e64490c161c30acf6f2a22a356705522a9&";
+  const chaiLG =
+    "https://cdn.discordapp.com/attachments/1096324843877703713/1200675000554901514/image.png?ex=65c70abb&is=65b495bb&hm=1f1f97369812429c98de662861d2946282e291ddb16e58f58581bb3ea55fb612&";
   React.useEffect(() => {
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
   const isLargeDevice = windowWidth >= 768;
@@ -60,7 +66,14 @@ const Mid = () => {
         <div className="w-[16.84rem] lg:w-[20.31rem] mt-[17px] font-thin relative text-[1.38rem] lg:text-[1.81rem] lg:text-[#767676]  text-darkslategray1 text-center ">{`29th - 31st March 2024 `}</div>
       </div>
 
-      <div className="w-[210px] mb-[88.6px] mx-auto mt-[106px] lg:mt-[140px] relative rounded-[40px] bg-white box-border h-[4.19rem] overflow-hidden text-left text-[1.13rem] text-black  font-[500] border-[0.3px] border-solid border-darkgray">
+      <div
+        onClick={() => {
+          const redirectUrl = "https://ethmumbai.in/";
+
+          window.open(redirectUrl, "_blank");
+        }}
+        className="w-[210px] hover:scale-105 transition-transform duration-150 cursor-pointer mb-[88.6px] mx-auto mt-[106px] lg:mt-[140px] relative rounded-[40px] bg-white box-border h-[4.19rem] overflow-hidden text-left text-[1.13rem] text-black  font-[500] border-[0.3px] border-solid border-darkgray"
+      >
         <div
           style={{ fontWeight: 700 }}
           className="absolute top-[1.38rem] left-[2.75rem] font-inter font-300"
--- src/page/Mainpage.jsx
+++ src/page/Mainpage.jsx
@@ -1,70 +1,91 @@
-import React from 'react'
-import logo from '../assets/logo.svg'
-import LogoEditor from '../components/LogoEditor'
-import './css/Mainpage.css'
-import Mid from '../components/Mid'
-import Footer from '../components/Footer'
+import React from "react";
+import logo from "../assets/logo.svg";
+import LogoEditor from "../components/LogoEditor";
+import "./css/Mainpage.css";
+import Mid from "../components/Mid";
+import Footer from "../components/Footer";
 
 const Mainpage = () => {
-    const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
-    const horizontalImage1_1='https://cdn.discordapp.com/attachments/1096324843877703713/1200569472164044871/image.png?ex=65c6a873&is=65b43373&hm=bb5d702ecec4eb7aebfaa8935d741aac235c6a3b9b3334127ed40760904a50ae&'
-    const horizontalImage1_2 ='https://cdn.discordapp.com/attachments/1096324843877703713/1200569332703432755/image.png?ex=65c6a852&is=65b43352&hm=7ed8dfb7413164cceec73c72b201def54e48ae50d1c745526cb0e3cf67895668&'
-    const horizontalImage2_1='https://cdn.discordapp.com/attachments/1096324843877703713/1200607126922473563/image.png?ex=65c6cb85&is=65b45685&hm=00181d6f576452d9f94b36dc4dc3f373aa3bd8c1f2b249b4d710bb62075a7aee&'
-    const horizontalImage2_2='https://cdn.discordapp.com/attachments/1096324843877703713/1200609698584469564/image.png?ex=65c6cdea&is=65b458ea&hm=a485c91d09401882086ba99c367d998d119726bacbe3da01583f9844bfc1792e&'
+  const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
+  const horizontalImage1_1 =
+    "https://cdn.discordapp.com/attachments/1096324843877703713/1200569472164044871/image.png?ex=65c6a873&is=65b43373&hm=bb5d702ecec4eb7aebfaa8935d741aac235c6a3b9b3334127ed40760904a50ae&";
+  const horizontalImage1_2 =
+    "https://cdn.discordapp.com/attachments/1096324843877703713/1200569332703432755/image.png?ex=65c6a852&is=65b43352&hm=7ed8dfb7413164cceec73c72b201def54e48ae50d1c745526cb0e3cf67895668&";
+  const horizontalImage2_1 =
+    "https://cdn.discordapp.com/attachments/1096324843877703713/1200607126922473563/image.png?ex=65c6cb85&is=65b45685&hm=00181d6f576452d9f94b36dc4dc3f373aa3bd8c1f2b249b4d710bb62075a7aee&";
+  const horizontalImage2_2 =
+    "https://cdn.discordapp.com/attachments/1096324843877703713/1200609698584469564/image.png?ex=65c6cdea&is=65b458ea&hm=a485c91d09401882086ba99c367d998d119726bacbe3da01583f9844bfc1792e&";
 
-    React.useEffect(() => {
-        const handleResize = () => {
-          setWindowWidth(window.innerWidth);
-        };
-    
-        window.addEventListener('resize', handleResize);
-    
-        return () => {
-          window.removeEventListener('resize', handleResize);
-        };
-      }, []);
-      const isLargeDevice = windowWidth >= 768;
+  React.useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  const isLargeDevice = windowWidth >= 768;
   return (
-    <div className='w-full  h-auto'>
+    <div className="w-full  h-auto">
+      {/* hero */}
+      <section className=" relative overflow-hidden h-auto flex flex-col bg-[#FFE7BA]">
+        <img
+          className="w-[166px] mx-auto lg:mx-1 my-5 relative  overflow-hidden h-[3.94rem] object-cover"
+          src={logo}
+          alt="Logo"
+        />
+        <img
+          className="absolute w-[526px] lg:w-[65.938rem] lg:h-[59rem] lg:left-[-11.613rem] lg:top-[2.563rem] overflow-hidden h-[471px] left-[-233px] top-[591px] object-cover"
+          src="https://cdn.discordapp.com/attachments/1096324843877703713/1200511965106294885/image.png?ex=65c672e4&is=65b3fde4&hm=ff0949a0a27f3f6231cbe87f60bfce83ce9414ad73fb012999c3af7135b79516&"
+          alt=""
+        />
+        <img
+          className="absolute w-[515px] lg:w-[56rem] lg:h-[56rem] lg:left-[46.25rem] lg:top-[8.625rem]  overflow-hidden right-[-180px] top-[597px] h-[32.19rem] object-contain"
+          alt=""
+          src="https://cdn.discordapp.com/attachments/1096324843877703713/1200511913596039188/image.png?ex=65c672d8&is=65b3fdd8&hm=7b8208cc5e5d3958fd8c3658700e69045c5f660880b09f36dd8a47f3671f7ad2&"
+        />
 
-        {/* hero */}
-        <section className=' relative overflow-hidden h-auto flex flex-col bg-[#FFE7BA]'>
-        <img className='w-[166px] mx-auto lg:mx-1 my-5 relative  overflow-hidden h-[3.94rem] object-cover' src={logo} alt="Logo" />
-        <img className='absolute w-[526px] lg:w-[65.938rem] lg:h-[59rem] lg:left-[-11.613rem] lg:top-[2.563rem] overflow-hidden h-[471px] left-[-233px] top-[591px] object-cover' src="https://cdn.discordapp.com/attachments/1096324843877703713/1200511965106294885/image.png?ex=65c672e4&is=65b3fde4&hm=ff0949a0a27f3f6231cbe87f60bfce83ce9414ad73fb012999c3af7135b79516&" alt="" />
-        <img className="absolute w-[515px] lg:w-[56rem] lg:h-[56rem] lg:left-[46.25rem] lg:top-[8.625rem]  overflow-hidden right-[-180px] top-[597px] h-[32.19rem] object-contain" alt="" src="https://cdn.discordapp.com/attachments/1096324843877703713/1200511913596039188/image.png?ex=65c672d8&is=65b3fdd8&hm=7b8208cc5e5d3958fd8c3658700e69045c5f660880b09f36dd8a47f3671f7ad2&" />
-
-        <div className='min-h-[1100px] lg:min-h-[864px]'>
-            <LogoEditor/>
+        <div className="min-h-[1100px] lg:min-h-[864px]">
+          <LogoEditor />
         </div>
+      </section>
+
+      {/* moveImage1
+       */}
+      <div className="w-full overflow-x-hidden">
+        <section className="relative w-[300%] lg:w-[500%] h-[71px] ">
+          <img
+            src={isLargeDevice ? horizontalImage1_2 : horizontalImage1_1}
+            id="horizontalImage1"
+            className="horizontalStrip1 absolute h-[71px]  object-cover "
+            alt="horizontalImg"
+          />
         </section>
+      </div>
 
-        {/* moveImage1
-         */}
-         <div className='w-full overflow-x-hidden'>
+      {/* mid portion */}
 
-         <section className='relative w-[300%] lg:w-[500%] h-[71px] '>
-            <img
-            src={isLargeDevice ? horizontalImage1_2 : horizontalImage1_1} id='horizontalImage1' className='horizontalStrip1 absolute h-[71px]  object-cover '  alt="horizontalImg" />
-         </section>
-            </div>
-
-            {/* mid portion */}
-            
-            <Mid/>
+      <Mid />
 
       {/*moveImage2  */}
-      <div className='w-full overflow-x-hidden'>
+      <div className="w-full overflow-x-hidden">
+        <section className="relative w-[300%] lg:w-[225%] h-[71px] ">
+          <img
+            src={isLargeDevice ? horizontalImage2_2 : horizontalImage2_1}
+            id="horizontalImage1"
+            className="horizontalStrip1 absolute h-[71px]  object-cover "
+            alt="horizontalImg"
+          />
+        </section>
+      </div>
 
-         <section className='relative w-[300%] lg:w-[225%] h-[71px] '>
-            <img
-            src={isLargeDevice ? horizontalImage2_2 : horizontalImage2_1} id='horizontalImage1' className='horizontalStrip1 absolute h-[71px]  object-cover '  alt="horizontalImg" />
-         </section>
-            </div>
-
-            {/* footer */}
-            <Footer/>
+      {/* footer */}
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Mainpage
+export default Mainpage;
